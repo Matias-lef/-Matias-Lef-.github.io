e84c5bf0b2a57794b9f19ae1f0220d7a281bf0ba
Matias-lef <pichilefmatias@gmail.com>
2024-11-15 15:59:04 -0300

    tp5

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Para manejar el campo radio button
     document.querySelectorAll('input[name="contact_method"]').forEach(input => {
         input.addEventListener('change', (event) => {
             document.getElementById('tabla_contact_method').textContent = event.target.value;
         });
     });
 
-    // Para manejar los checkboxes
     document.querySelectorAll('input[name="subscription_type"]').forEach(input => {
         input.addEventListener('change', () => {
             const selectedSubscriptions = Array.from(document.querySelectorAll('input[name="subscription_type"]:checked'))
